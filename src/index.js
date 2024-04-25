338240f956b79baca8fe087b0ddab2985cce14a9
christopherleavitt99 <cml95@byui.edu>
2024-04-25 15:47:16 -0600

    initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+var firstName = 'Inigo';
+var lastName = 'Montoya';
+function sayHello(name) {
+    console.log("Hello, my name is ".concat(name, "! You killed my father. Prepare to die."));
+}
+sayHello(firstName);
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const firstName: string = 'Inigo';
+const lastName: string = 'Montoya';
+
+function sayHello(name: string){
+    console.log(`Hello, my name is ${name}! You killed my father. Prepare to die.`);
+}
+
+sayHello(firstName); 
