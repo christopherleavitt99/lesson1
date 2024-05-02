bfa1e04f825586e584bd19007d28b60fe839630e
christopherleavitt99 <cml95@byui.edu>
2024-05-02 15:51:56 -0600

    js update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 var firstName = 'Inigo';
 var lastName = 'Montoya';
-function sayHello(name) {
-    console.log("Hello, my name is ".concat(name, "! You killed my father. Prepare to die."));
+function sayHello() {
+    console.log("Hello, my name is ".concat(firstName, "! You killed my father. Prepare to die."));
 }
-sayHello(firstName);
+document.getElementById("name").addEventListener("click", sayHello);
+
+
+// 1. How does the function now when to be ran?
+
+// 2. How do I connect the button to the function?
